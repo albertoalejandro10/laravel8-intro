c020ee882000acd729d1bc7c1099eda7e0e66894
Alberto Alejandro <alberto196g@gmail.com>
2021-03-24 00:47:47 -0400

    Starting the main course container

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
     use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+
+    public function getExcerptAttribute(){
+        return substr($this->description, 0, 80)."...";
+    }
+
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Últimos Cursos</h1>
+    <h2 class="text-xl text-gray-600">Fórmate Online como profesional en Tecnología.</h2>
+    <h3 class="text-lg text-gray-600">70% de los graduados duplican sus ingresos.</h3>
+</div>
+<livewire:course-list>
+@endsection
